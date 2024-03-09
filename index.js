7ea2e69ee46affe23f6cb710e67693a6bc95a32e
MOHAMED <mohamedelafandy593@gmail.com>
2024-03-09 17:30:57 +0200

    'farm'

--- index.js
+++ index.js
@@ -30,61 +30,61 @@ const dataObj = JSON.parse(data);
 // const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
 // console.log(slugs);
 
-server.on("request", (req, res) => {
-  const { query, pathname } = url.parse(req.url, true);
+// server.on("request", (req, res) => {
+//   const { query, pathname } = url.parse(req.url, true);
 
-  // Overview page
-  if (pathname === '/overview') {
-    res.writeHead(200, {
-      'Content-type': 'text/html',
-    });
+//   // Overview page
+//   if (pathname === '/overview') {
+//     res.writeHead(200, {
+//       'Content-type': 'text/html',
+//     });
 
-    const cardsHtml = dataObj
-      .map((el) => replaceTemplate(tempCard, el))
-      .join('');
-    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-    res.end(output);
+//     const cardsHtml = dataObj
+//       .map((el) => replaceTemplate(tempCard, el))
+//       .join('');
+//     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+//     res.end(output);
 
-    // Product page
-  } else if (pathname === '/product') {
-    res.writeHead(200, {
-      'Content-type': 'text/html',
-    });
-    const product = dataObj[query.id];
-    const output = replaceTemplate(tempProduct, product);
-    res.end(output);
-  } else if (pathname === '/index.html') {
-    res.writeHead(200, {
-      'Content-type': 'text/html',
-    });
-    res.end(templanding);
+//     // Product page
+//   } else if (pathname === '/product') {
+//     res.writeHead(200, {
+//       'Content-type': 'text/html',
+//     });
+//     const product = dataObj[query.id];
+//     const output = replaceTemplate(tempProduct, product);
+//     res.end(output);
+//   } else if (pathname === '/index.html') {
+//     res.writeHead(200, {
+//       'Content-type': 'text/html',
+//     });
+//     res.end(templanding);
 
-    // Shopping cart page
-  } else if (pathname === '/shopping-cart') {
-    res.writeHead(200, {
-      'Content-type': 'text/html',
-    });
-    const product = dataObj[query.id];
-    const output = replaceTemplate(tempShopping, product);
-    res.end(output);
+//     // Shopping cart page
+//   } else if (pathname === '/shopping-cart') {
+//     res.writeHead(200, {
+//       'Content-type': 'text/html',
+//     });
+//     const product = dataObj[query.id];
+//     const output = replaceTemplate(tempShopping, product);
+//     res.end(output);
 
-    // API
-  } else if (pathname === '/api') {
-    res.writeHead(200, {
-      'Content-type': 'application/json',
-    });
-    res.end(data);
+//     // API
+//   } else if (pathname === '/api') {
+//     res.writeHead(200, {
+//       'Content-type': 'application/json',
+//     });
+//     res.end(data);
 
-    // Not found
-  } else {
-    res.writeHead(404, {
-      'Content-type': 'text/html',
-      'my-own-header': 'hello-world',
-    });
-    res.end('<h1>Page not found!</h1>');
-  }
-});
-
-// server.listen(5500, '127.0.0.1', () => {
-//   console.log('Listening to requests on port 8000');
+//     // Not found
+//   } else {
+//     res.writeHead(404, {
+//       'Content-type': 'text/html',
+//       'my-own-header': 'hello-world',
+//     });
+//     res.end('<h1>Page not found!</h1>');
+//   }
 // });
+
+// // server.listen(5500, '127.0.0.1', () => {
+// //   console.log('Listening to requests on port 8000');
+// // });
